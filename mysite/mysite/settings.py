ac0c54a6969597904ce5636fb7633c38e09f07a9
mohammadimrans09t <mohammadimrans09t@gmail.com>
2025-01-09 13:08:16 +0000

    disabled cors

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -24,12 +24,14 @@ CSRF_TRUSTED_ORIGINS = [
     'https://8000-idx-easygrocery-1735744862938.cluster-7ubberrabzh4qqy2g4z7wgxuw2.cloudworkstations.dev',
     'https://3000-idx-easygrocery-1735717909998.cluster-fu5knmr55rd44vy7k7pxk74ams.cloudworkstations.dev',
     'https://easygrocery-server.onrender.com',
+    'https://easygrocery.onrender.com'
 ]
 
 CORS_ALLOWED_ORIGINS = [
     "https://3000-idx-easygrocery-1735717909998.cluster-fu5knmr55rd44vy7k7pxk74ams.cloudworkstations.dev",
     'https://3000-idx-easygrocery-1735717909998.cluster-fu5knmr55rd44vy7k7pxk74ams.cloudworkstations.dev',
     'https://easygrocery-server.onrender.com',
+    'https://easygrocery.onrender.com'
 ]
 
 
@@ -51,12 +53,14 @@ INSTALLED_APPS = [
     'product',
 ]
 
+CSRF_COOKIE_SECURE = False
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
