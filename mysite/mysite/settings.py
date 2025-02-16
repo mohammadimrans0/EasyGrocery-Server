ed561495a669a3fc80ba12122ba3242cb1eba0d9
Mohoammad Imran <mohoammadimran@Mohammads-MacBook.local>
2025-02-16 22:38:40 +0600

    added vercel.json

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -86,7 +86,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'mysite.wsgi.application'
+WSGI_APPLICATION = 'mysite.wsgi.app'
 
 
 # Database
@@ -135,6 +135,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
