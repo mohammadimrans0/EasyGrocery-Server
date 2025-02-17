89614dfeaee9ef49ee20e9f214437b3f15561300
Mohoammad Imran <mohoammadimran@Mohammads-MacBook.local>
2025-02-17 15:14:25 +0600

    fix cors issue

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -30,7 +30,17 @@ CSRF_TRUSTED_ORIGINS = [
     'http://127.0.0.1:8000',
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    'https://8000-idx-easygrocery-1735744862938.cluster-7ubberrabzh4qqy2g4z7wgxuw2.cloudworkstations.dev',
+    'https://3000-idx-easygrocery-1735717909998.cluster-fu5knmr55rd44vy7k7pxk74ams.cloudworkstations.dev',
+    'https://easygrocery-server.onrender.com',
+    'https://easygrocery-server.vercel.app',
+    'https://easygrocery.vercel.app',
+    'http://localhost:3000',
+    'http://127.0.0.1:8000',
+]
+
+# CORS_ALLOW_ALL_ORIGINS = True
 
 
 # Application definition
